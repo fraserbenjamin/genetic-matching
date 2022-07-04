12a472eb13993cb07c27dec8355189050c0b0455
Fraser Benjamin <fraser.benjamin@outlook.com>
2022-07-04 11:26:14 +0100

    Added drag and drop

--- src/components/UploadSpreadsheet.tsx
+++ src/components/UploadSpreadsheet.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { DragEvent, useState } from "react";
 import ReactSpreadsheet from "react-spreadsheet";
 import * as xlsx from "xlsx";
 import { IPreview } from "../types";
@@ -21,6 +21,22 @@ const UploadSpreadsheet: React.FC<IProps> = ({ onLoad, onClose }) => {
             });
         }
     }
+
+    const handleDrop = (event: DragEvent<HTMLLabelElement>) => {
+        console.log("drop")
+        console.log(event)
+        event.preventDefault();
+        Array.from(event.dataTransfer.items).forEach((item: DataTransferItem) => {
+            if (item.kind === 'file') {
+                const file = item.getAsFile();
+                if(file) readFile(file).then(data => {
+                    console.log(data);
+                    setPreview(data);
+                });
+            }
+        });
+    }
+
 
     const csvToArray = (csv: string): string[][] => {
         const rows = csv.split("\n");
@@ -80,7 +96,7 @@ const UploadSpreadsheet: React.FC<IProps> = ({ onLoad, onClose }) => {
                             </div>
                         ) : (
                             <div className="flex justify-center items-center w-full">
-                                <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <label onDrop={handleDrop} onDragOver={(e) => e.preventDefault()} htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                     <div className="flex flex-col justify-center items-center pt-5 pb-6">
                                         <svg className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
                                         <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
